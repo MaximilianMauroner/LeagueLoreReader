ab0343fa11976ca7ab738d513b24d42fdc482a41
Maximilian <maximilian@mauroner.eu>
2022-03-27 16:44:07 +0200

    started implementing a better backend using the riot "unnoficial" api to get my data, todo need to workout how to update it efficiently

---
+++ Backend_v2/database/seeders/LocationSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Location;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\VarDumper\VarDumper;
+
+class LocationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $location_res = Http::get('https://universe-meeps.leagueoflegends.com/v1/en_us/faction-browse/index.json');
+        if ($location_res->ok()) {
+            $locations = $location_res->json();
+            foreach ($locations['factions'] as $location) {
+                Location::create([
+                    "title" => $location["name"],
+                    "slug" => $location["slug"],
+                    "description" => "",
+                    "url" => "",
+                    "image_url" => $location['image']["uri"]
+                ]);
+            }
+        }
+        Location::create([
+            "title" => "Unaffiliated",
+            "slug" => "unaffiliated",
+            "description" => "",
+            "url" => "",
+            "image_url" => ""
+        ]);
+    }
+}
