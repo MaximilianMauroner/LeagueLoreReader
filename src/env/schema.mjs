bf60d1e50734168eb0440c59159b263193d41cc7
Maximilian Mauroner <maximilian@mauroner.eu>
2022-09-10 17:07:17 +0200

    try 4

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -29,8 +29,7 @@ export const serverSchema = z.object({
  * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 export const clientSchema = z.object({
-    NEXT_PUBLIC_PROD_FILE_PATH: z.string(),
-    NEXT_PUBLIC_DEV_FILE_PATH: z.string(),
+    NEXT_PUBLIC_FILE_PATH: z.string(),
 });
 
 /**
@@ -40,6 +39,5 @@ export const clientSchema = z.object({
  * @type {{ [k in keyof z.infer<typeof clientSchema>]: z.infer<typeof clientSchema>[k] | undefined }}
  */
 export const clientEnv = {
-    NEXT_PUBLIC_DEV_FILE_PATH: process.env.NEXT_PUBLIC_DEV_FILE_PATH,
-    NEXT_PUBLIC_PROD_FILE_PATH: process.env.NEXT_PUBLIC_PROD_FILE_PATH,
+    NEXT_PUBLIC_FILE_PATH: process.env.NEXT_PUBLIC_FILE_PATH
 };
